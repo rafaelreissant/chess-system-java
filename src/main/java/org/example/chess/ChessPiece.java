1f1170b1962b3dc5d9a542937ad923c2b84a9fa5
rafaelreissant <santireis14@gmail.com>
2025-11-24 10:26:18 -0300

    Possible moves of a piece

--- src/main/java/org/example/chess/ChessPiece.java
+++ src/main/java/org/example/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package org.example.chess;
 import org.example.boardGame.Board;
 import org.example.boardGame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 
     private Color color;
 
